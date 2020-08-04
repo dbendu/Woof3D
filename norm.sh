e70bbb6589fbf2d497378fcf5ac60577253d739c
Mazie Burl <mburl@oa-b1.msk.21-school.ru>
2020-08-04 16:16:09 +0300

    normed

---
+++ norm.sh
@@ -0,0 +1,33 @@
+clear
+echo "== Made by mburl ==="
+echo "=== Welcome to script that checks for norm errors ==="
+echo
+echo "Files/Folders that will be checked"
+ls
+echo
+ans="*"
+global_bool=0
+echo "Show all files in folder that didn't pass the norm?"
+read showing
+for file in $ans
+do
+	if ! norminette $file | grep "Error" > /dev/null ; then
+		echo $file "\033[32mV\033[0m"
+	else
+		echo $file "\033[31mX\033[0m"
+		case "$ans" in
+                "y"|"Y"|"Yes"|"yes")
+                        norminette $file
+                        ;;
+                "n"|"N"|"no"|"No")
+                        ;;
+        esac
+		global_bool=1
+	fi
+	echo "------------------------"
+done
+if [ $global_bool -eq 1 ]; then
+	echo "\033[31m== DONE Errors found ==\033[0m"
+else
+	echo "\033[32m== DONE No errors found ==\033[0m"
+fi
